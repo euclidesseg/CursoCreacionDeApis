af8dd66d9fdda9ee5c6801fbf328298a8cd7e8c7
euclidesseg <euder0206@gmail.com>
2023-01-02 23:31:09 -0500

    se agrega la arquitectura de la api

---
+++ BancoApiRes/Aplication/ServiceExtensions.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Aplication
+{
+    public static  class ServiceExtensions
+    {
+        /*control+rg para borrar los using innesesarios*/
+        public static void AddaplicationLayer(this IServiceCollection services) 
+        {
+            /*ahora el llamado de matricula de servicios*/
+            services.AddAutoMapper(Assembly.GetExecutingAssembly()); /* Esto me permite decirle que registre automatidcamente qu 
+                                                                      *los mapeos que haga en la biblioteca de clases*/
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            /* Este tipo de registro se deberia hacer en el startApp de la aplicacion de netcore pero no dependemos de eso ya que usamos 
+             * el paquete de dependencias de inyeccion*/
+
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            /*Todas las sentencias de validacion iran dentro de la viblioteca de clase Aplication*/
+            
+        }
+    }
+}
